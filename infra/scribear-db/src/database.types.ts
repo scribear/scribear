0963e887e8bc6f36053026e3e6644c08306058ee
Bennett Wu <57691028+bennettrwu@users.noreply.github.com>
2026-02-04 16:59:14 -0600

    fix: push updated db types

--- infra/scribear-db/src/database.types.ts
+++ infra/scribear-db/src/database.types.ts
@@ -3,4 +3,17 @@
  * Please do not edit it manually.
  */
 
-export interface DB {}
+import type { ColumnType } from "kysely";
+
+export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
+  ? ColumnType<S, I | undefined, U>
+  : ColumnType<T, T | undefined, T>;
+
+export interface Kiosks {
+  id: Generated<string>;
+  secret_hash: string;
+}
+
+export interface DB {
+  kiosks: Kiosks;
+}
