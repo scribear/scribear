a6076177283ae093722bb206b31b87306427d3e0
Bennett Wu <57691028+bennettrwu@users.noreply.github.com>
2026-02-06 13:52:59 -0600

    fix: bad merge

--- apps/session-manager/src/server/dependency-injection/register-dependencies.ts
+++ apps/session-manager/src/server/dependency-injection/register-dependencies.ts
@@ -25,13 +25,9 @@ interface AppDependencies extends BaseDependencies {
   dbClientConfig: DBClientConfig;
   dbClient: DBClient;
 
-  // Database
-  dbClient: DBClient;
-
   // Services
   jwtServiceConfig: JwtServiceConfig;
   jwtService: JwtService;
-
 
   // Healthcheck
   healthcheckController: HealthcheckController;
